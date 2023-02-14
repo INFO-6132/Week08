0f0a853baa2eef4bc60b3b762939bcf82a0ef2aa
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2023-02-14 09:32:17 -0500

    Install modifications for redux persist

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 
 import * as React from 'react';
-
 import TodoApp from './screens/TodoApp';
+import { Text, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 
 // Import Redux
-import store from './redux/store';
+import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <TodoApp/>
+      <PersistGate loading={<Text>Loading View</Text>} persistor={persistor}>
+        <TodoApp/>
+      </PersistGate>
     </Provider>
   );
 }
